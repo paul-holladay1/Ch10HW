69f1002783863819613e3ecec7825cafe55de55a
Paul <paul_holladay1@baylor.edu>
2022-09-26 17:40:12 -0500

    Employee Class updates

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -1,3 +1,7 @@
+# 2) EmployeeClass.py - Write a class named Employee that holds the following data about an employee in attributes: name, ID number, 
+# department, job title and monthly salary.The Employee class’s __init__ method should accept an argument for each attribute. 
+# The Employee class should have accessor methods for each attribute. All attribute should be hidden.
+
 class Employee:
 
     def __init__(self, name, ID, department, job, salary):
@@ -7,17 +11,32 @@ class Employee:
         self.__job = job
         self.__salary = salary
     
+    def setName(self, name):
+        self.__name = name
+
     def getName(self):
         return self.__name
     
+    def setID(self, ID):
+        self.__ID = ID
+
     def getID(self):
         return self.__ID
     
+    def setDepartment(self, department):
+        self.__department = department
+
     def getDepartment(self):
         return self.__department
     
+    def setJob(self, job):
+        self.__job = job
+
     def getJob(self):
         return self.__job
     
-    def getJob(self):
+    def setSalary(self, salary):
+        self.__salary = salary
+
+    def getSalary(self):
         return self.__salary
