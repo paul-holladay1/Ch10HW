fa3251be520ad209765d51185debdfa8d64c618f
Paul <paul_holladay1@baylor.edu>
2022-09-26 23:28:51 -0500

    Tables

--- NetPay.py
+++ NetPay.py
@@ -13,6 +13,22 @@ charge3 = p.PayrollDeduction('food court', '8/17/2022', 15.25, 21547)
 charge4 = p.PayrollDeduction('vending machine', '8/22/2022', 3.00, 58475)
 charge5 = p.PayrollDeduction('vending machine', '8/5/2022', 2.75, 58475)
 
+from tabulate import tabulate
+table = [['Name', 'ID Number', 'Department', 'Job Title', 'Monthly Salary'], 
+         [employee.getName(), employee.getID(), employee.getDepartment(), employee.getJob(), employee.getSalary()]]
+print(tabulate(table))
+print()
+
+table = [['Description', 'Date', 'Charge', 'EmployeeID'],
+         [p.PayrollDeduction.getDescription(charge1), p.PayrollDeduction.getDate(charge1), p.PayrollDeduction.getCharge(charge1), p.PayrollDeduction.getEmployeeID(charge1)],
+         [p.PayrollDeduction.getDescription(charge2), p.PayrollDeduction.getDate(charge2), p.PayrollDeduction.getCharge(charge2), p.PayrollDeduction.getEmployeeID(charge2)],
+         [p.PayrollDeduction.getDescription(charge3), p.PayrollDeduction.getDate(charge3), p.PayrollDeduction.getCharge(charge3), p.PayrollDeduction.getEmployeeID(charge3)],
+         [p.PayrollDeduction.getDescription(charge4), p.PayrollDeduction.getDate(charge4), p.PayrollDeduction.getCharge(charge4), p.PayrollDeduction.getEmployeeID(charge4)],
+         [p.PayrollDeduction.getDescription(charge5), p.PayrollDeduction.getDate(charge5), p.PayrollDeduction.getCharge(charge5), p.PayrollDeduction.getEmployeeID(charge5)]]
+         
+print(tabulate(table))
+print()
+
 print('*** Employee Pay ***')
 print('Name: ', employee.getName())
 print('ID Number', employee.getID())
